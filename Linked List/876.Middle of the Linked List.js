6a90f30514994f1195526aa2185aaf4a5ac80888
liwanting <1808054933@qq.com>
2022-06-01 09:49:42 +0800

    Add Q876

---
+++ Linked List/876.Middle of the Linked List.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+ var middleNode = function(head) {
+  /**
+  * 快慢指针法：慢指针一次走一步，快指针一次走两步，快指针到链表结尾时，慢指针在中点位置
+  */
+  let slow = head;
+  let fast = head;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  return slow;
+};
