a3fc3a367eeb05540fb01575daf1a71dd935a1a2
liwanting <1808054933@qq.com>
2022-06-04 01:04:38 +0800

    Add Q27

---
+++ Array/27.Remove Element.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+ var removeElement = function(nums, val) {
+  /**
+   * 双指针法
+   */
+  let slow = 0;
+  let fast = 0;
+  while (fast < nums.length) {
+    if (nums[fast] !== val) {
+      nums[slow] = nums[fast];
+      slow++;
+    } 
+    fast++;
+  }
+  return nums;
+};
