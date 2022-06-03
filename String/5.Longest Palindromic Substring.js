084b1175ac9f7e862d131aff81ef1cf247d99de2
liwanting <1808054933@qq.com>
2022-06-03 23:56:02 +0800

    Add Q05

--- String/5.Longest Palindromic Substring.js
+++ String/5.Longest Palindromic Substring.js
@@ -3,6 +3,10 @@
  * @return {string}
  */
  var longestPalindrome = function(s) {
+  /**
+   * 双指针法：从中央向两边扩散
+   * 注意：考虑奇数和偶数两种情况
+   */
   let result = '';
   for (let i = 0; i < s.length; i++) {
     const s1 = palindrome(s, i, i);
